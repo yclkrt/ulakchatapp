020eb3df5d564a6e20f28fb7c5fc7b4f3c2ed7e8
Yücel KURT <yclkrt4434@gmail.com>
2026-09-16 01:51:17 +0300

    uygulama firebase'e bağlandı. Gerekli kurulumlar tamamlandı. main.dart dosyasında firebase için initialize işlemi de yapıldı.

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:lingo_easy/lingo_easy.dart';
+import 'package:ulakchatapp/firebase_options.dart';
 
 import 'core/router/app_router.dart';
 import 'core/theme/app_theme.dart';
 import 'core/theme/theme_provider.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(
     const Directionality(
       textDirection: TextDirection.ltr,
